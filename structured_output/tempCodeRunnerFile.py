6f1e9436b4ed1f176252b65f706628363ef6e477
itzmit49 <itzmit49@gmail.com>
2026-07-01 17:04:45 +0530

    updated

---
+++ structured_output/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+le": "Review",
+  "type": "object",
+  "properties": {
+    "key_themes": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the key themes discussed in the review in a list"
+    },
+    "summary": {
+      "type": "string"
